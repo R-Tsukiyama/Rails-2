fbdf277dc2aa0a8e545bf8734e313616d5ea3359
tsukki <tsukki@tsukkinoMacBook-Air.local>
2023-06-14 15:59:31 +0900

    users&application-contrller記述、Userモデルにバリデーション作成、home-view記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
+  end
+
+  def maypage
+    redirect_to user_path(current_user)
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:notice] = "更新しました"
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :username,
+      :profile,
+      :image
+    )
   end
 end
