8993cbba0f89c9d4aaa5c228e9bc3c8303bd3cb7
tsukki <tsukki@tsukkinoMacBook-Air.local>
2023-06-14 16:44:39 +0900

    Roomモデル作成

---
+++ db/migrate/20230614074341_create_rooms.rb
@@ -0,0 +1,14 @@
+class CreateRooms < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.string :use_id
+      t.string :image
+      t.introduction :text
+      t.string :address
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
