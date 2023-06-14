b299b7918e3504ade44066e0c957952a368a8b96
tsukki <tsukki@tsukkinoMacBook-Air.local>
2023-06-14 19:38:12 +0900

    romms-controller実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,67 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
   def index
+    @rooms = current_user.rooms #ログイン中のユーザーの情報のみ
   end
+
+  def new
+    @room = current_user.rooms.new
+  end
+
+  def create
+    @room = current_user.rooms.new(room_params)
+    if @room.save
+      flash[:notice] = "宿泊施設を登録しました"
+      redirect_to  @room
+    else
+      flash[:failure] = "問題が発生しました"
+      render :new
+    end
+  end
+
+  def show
+    @user = current_user
+    @room = Room.find(params[:id])
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      flash[:notice] = "宿泊施設を更新しました"
+      redirect_to @room
+    else
+      flash[:failure] = "問題が発生しました"
+      render :edit
+    end
+  end
+ 
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    flash[:notice] = "宿泊施設を削除しました"
+    redirect_to :rooms
+  end
+
+  def search
+  end
+
+  private
+  def room_params
+    params.require(:room)
+    .permit(
+      :name,
+      :introduction,
+      :price,
+      :address,
+      :image,
+    )
+    .merge(
+      user_id :current_user.id
+    )
+    #mergeメソッド・・・ユーザーのidを直接入力しないがパラメーターに含めてDBの保存ができる。
 end
--- db/migrate/20230614074341_create_rooms.rb
+++ db/migrate/20230614074341_create_rooms.rb
@@ -2,12 +2,12 @@ class CreateRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :rooms do |t|
       t.string :name
-      t.string :use_id
+      t.string :user_id
       t.string :image
-      t.introduction :text
+      t.text :introduction
       t.string :address
       t.integer :price
-
+      
       t.timestamps
     end
   end
