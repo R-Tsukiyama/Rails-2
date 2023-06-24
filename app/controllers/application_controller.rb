3fb76d4b94a67c29926c07150e7203760c7bb84f
tsukki <tsukki@tsukkinoMacBook-Air.local>
2023-06-24 12:43:19 +0900

    ユーザー関連：html作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   #クラスの外からは呼び出せない。同じインスタンス内で呼び出せる。他のインスタンスでも同じクラスやサブクラスであれば呼び出せる。
     
   def configure_permitted_parameters
-  #サインアップ時にusernameのストロングパラメータを追加
+  #サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  #アカウント編集時にusernameとprofileのストロングパラメータを追加
+  #アカウント編集時にnameとprofileのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile])
   end
 end
